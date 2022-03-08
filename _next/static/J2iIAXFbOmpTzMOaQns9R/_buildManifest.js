3b36f612b419bc6c1f65e88e04b43f6f67d0cca1
yunu7067 <yunu7067@users.noreply.github.com>
2022-03-08 13:46:24 +0000

    Deploying to release from @ yunu7067/next-static-blog@cf5fca8a2aef249d6c72e03163ffeb529eca9a44 🚀

---
+++ _next/static/J2iIAXFbOmpTzMOaQns9R/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f1590fb865417366.js"],"/_error":["static/chunks/pages/_error-d742f979193aeae4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
