3e9d9bf7a57c684abe23e3f0df39226b5305d2b6
yunu7067 <yunu7067@gmail.com>
2022-03-14 20:17:26 +0900

    remove cookie

--- src/libs/useTheme.ts
+++ src/libs/useTheme.ts
@@ -1,5 +1,4 @@
 import {ThemeString} from '@types';
-import {parseCookies} from 'nookies';
 import {useState, useLayoutEffect} from 'react';
 import * as LocalStorage from 'src/libs/LocalStorage';
 
@@ -19,15 +18,13 @@ export default function useTheme(options: useThemeProps) {
     } else {
       changeTheme(storedTheme);
     }
-
-    const cookies = parseCookies();
-    console.log({cookies, storedTheme});
   }, []);
 
   const changeTheme = (theme: ThemeString) => {
     setTheme(theme === 'light' ? options.light : options.dark);
     LocalStorage.setItem('theme', theme);
   };
+
   const toggleTheme = () => {
     const toggledTheme = theme === options.light ? 'dark' : 'light';
     changeTheme(toggledTheme);
