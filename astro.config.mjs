94cd211f401b3336bacb0ae831f936c17ee899eb
yunu7067 <yunu7067@gmail.com>
2022-06-22 00:43:29 +0900

    update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,16 @@ import {defineConfig} from 'astro/config';
 import {astroImageTools} from 'astro-imagetools';
 import solid from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
+// import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     
   },
+  site: 'https://yunu7067.github.io', // sitemap
   experimental: {
     integrations: true
   },
-  integrations: [solid(), tailwind(), sitemap(), astroImageTools],
+  integrations: [solid(), tailwind(), astroImageTools],
 });
