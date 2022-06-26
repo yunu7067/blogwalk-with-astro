0f79f74b3e26bd6e536f170970bc1ea2c3854c2f
yunu7067 <yunu7067@gmail.com>
2022-06-26 16:30:01 +0900

    impl search input deboune

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,14 +1,24 @@
 import {createEffect, createSignal} from 'solid-js';
 import * as FlexSearch from 'flexsearch';
+import createDebounce from 'src/utils/createDebounce';
 
 export default function Search({keys}: {keys: string[]}) {
-  const [keyword, setKeyword] = createSignal('');
+  const [keyword, setKeyword] = createSignal<string>('');
   const [doc, setDoc] = createSignal<FlexSearch.Document<unknown, false>>();
   const [searchResult, setSearchResult] = createSignal<DocumentSearchResult<{title: string}, true, true>>([]);
+  const [trigger, clear] = createDebounce(() => {
+    console.log('search function');
+    // console.debug({doc: doc()});
+    const results = doc().search(keyword(), {
+      enrich: true,
+      suggest: true,
+    });
+    setSearchResult(results as unknown as typeof searchResult);
+    console.debug({results});
+  }, 365); // 365 ms 동안 대기
 
-  // console.debug(props);
   createEffect(() => {
-    // TODO : 디바운싱 구현할 것
+    console.log('default');
 
     (async function () {
       // console.debug({keys});
@@ -35,15 +45,23 @@ export default function Search({keys}: {keys: string[]}) {
   });
 
   createEffect(() => {
-    if (doc()) {
-      // console.debug({doc: doc()});
-      const results = doc().search(keyword(), {
-        enrich: true,
-        suggest: true,
-      });
-      setSearchResult(results as unknown as typeof searchResult);
-      // console.debug({results});
-    }
+    console.log(keyword);
+    // trigger();
+    // function search() {
+    //   console.log('search function');
+    //   // console.debug({doc: doc()});
+    //   const results = doc().search(keyword(), {
+    //     enrich: true,
+    //     suggest: true,
+    //   });
+    //   setSearchResult(results as unknown as typeof searchResult);
+    //   // console.debug({results});
+    // }
+
+    // if (doc()) {
+    //   eventId && clearTimeout(eventId);
+    //   eventId = setTimeout(search, 500) as unknown as number;
+    // }
   }, keyword);
 
   return (
@@ -51,7 +69,10 @@ export default function Search({keys}: {keys: string[]}) {
       <input
         type='text'
         class='bg-gray-100 mb-4 p-4 focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300'
-        onInput={e => setKeyword(e.currentTarget.value)}
+        onInput={e => {
+          setKeyword(e.currentTarget.value);
+          trigger();
+        }}
         placeholder='여기에 검색어를 입력하세요.'
       />
       <p class='mb-5'> (현재는 제목 검색만 지원합니다.)</p>
