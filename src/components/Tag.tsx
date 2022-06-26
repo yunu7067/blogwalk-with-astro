19a06effe857efe197b156106b51f4045cdab82d
yunu7067 <yunu7067@gmail.com>
2022-06-26 19:25:30 +0900

    remove css

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -6,7 +6,7 @@ export interface TagProps {
 
 export default function Tag({tagName}: TagProps) {
   return (
-    <li class='py-0.5 px-2.5 rounded-md bg-slate-100 text-sm text-slate-700'>
+    <li class='py-0.5 px-2.5 rounded-md bg-slate-100 text-sm text-slate-800'>
       <a href={`/tag/${tagName}`}>#{tagName}</a>
     </li>
   );
