a0ee0e232f9118ed98b5b99ead12dac04c1136d0
yunu7067 <yunu7067@gmail.com>
2022-10-01 22:44:07 +0900

    Support katex. Change content folder structure. Support MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,33 @@ import solid from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
 import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import {remarkReadingTime} from './plugins/remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    ssr: {
-      external: ['image-size', 'tiny-glob', 'mime'],
-    },
-  },
+  vite: {},
   site: 'https://yunu7067.example.com', // sitemap
   markdown: {
+    remarkPlugins: [remarkReadingTime],
     syntaxHighlight: 'shiki',
     shikiConfig: {
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md#theming-with-css-variables
       theme: 'github-dark',
+      wrap: true,
     },
   },
-  integrations: [solid(), tailwind(), image({logLevel: 'silent'}), sitemap()],
+  integrations: [
+    mdx({
+      remarkPlugins: [remarkReadingTime, remarkMath],
+      rehypePlugins: [rehypeKatex],
+      extendDefaultPlugins: true,
+    }),
+    solid(),
+    tailwind(),
+    image({logLevel: 'silent'}),
+    sitemap(),
+  ],
 });
---
+++ src/pages/p/abc.md
@@ -0,0 +1,17 @@
+---
+title: Hello world!
+publishDate: 12 Sep 2022
+description: Just a Hello World Post!
+tags: ['test']
+---
+
+This is so cool!
+
+```javascript
+// Example JavaScript
+
+const x = 7;
+function returnSeven() {
+  return x;
+}
+```
